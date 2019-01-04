5aafa59658f3426e9ea06e3f528b8f1587ff769b
吕杭 <v-lvhang@chinayasha.com>
2019-01-04 10:22:21 +0800

    上传测试controller

--- src/main/java/com/ys/springboot_test/SpringbootTestApplication.java
+++ src/main/java/com/ys/springboot_test/SpringbootTestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +16,12 @@ import javax.annotation.Resource;
 
 @SpringBootApplication
 @ImportResource("classpath:dubbo.xml")
+@Controller
 public class SpringbootTestApplication {
 	@Resource
 	private Environment environment;
 	@ResponseBody
+	@GetMapping("/index")
 	String index(){
 		String a = environment.getProperty("name");
 		return "name:"+environment.getProperty("name");
